8f2b5e46b422d8b5ef28e60a103cf55e2130eb2a
Daniel Huong <daniel.huong86@gmail.com>
2017-10-12 16:07:51 +1100

    before challenges

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
     def format_cents(cents)
         Money.new(cents, "AUD").format
     end
-end
+
+    def format_cents_for_country(cents, country)
+        money = Money.new(cents, country.currency.iso_code)
+        money.format
+    end
+
+    end
